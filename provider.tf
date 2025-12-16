681313200eea26a7245afd7f2032220f758eb3cf
Mike Pietruszka <mike.pietruszka@sada.com>
2025-12-16 12:01:57 -0600

    Added provider so spacelift knows what to do

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "google" {
   project = "sada-mpietruszka-dev"
   region  = "us-central1"
 }
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
